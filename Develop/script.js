1027f86ec98951fae9af5e0c5ff3a3aa8185c71c
Opal <guerre5lily@gmail.com>
2021-10-19 22:42:10 -0700

    add length prompt and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
-
+var writePassword = function() {
+  var promptChooseLength = window.prompt("How long would you like your password to be? Please choose a number between 8 and 128")
+  //validate prompt characters
+  if (promptChooseLength === "" || promptChooseLength === null || promptChooseLength < 8 || promptChooseLength > 128) {
+    window.alert("Please choose a valid password length. Password must between 8 and 128 characters")
+    return writePassword()
+  }
+  confirmLower = window.confirm("If you'd like to include lower case letters, please click 'OK'")
+  confirmUpper = window.confirm("If you'd like to include upper case letters, please click 'OK'")
+  confirmNum = window.confirm("If you'd like to include numbers, please click 'OK'")
+  confirmSpecial = window.confirm("If you'd like to include special characters, please click 'OK'")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +26,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
